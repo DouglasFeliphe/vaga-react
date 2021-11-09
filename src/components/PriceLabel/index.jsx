d46450b3457c4090eb47111abc044a7878b48700
DouglasFeliphe <douglasfeliphe@hotmail.com>
2021-11-08 23:47:44 -0300

    Pequenos ajustes

--- src/components/PriceLabel/index.jsx
+++ src/components/PriceLabel/index.jsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-import Box from '@material-ui/core/Box'
+import Box from "@material-ui/core/Box";
 
 function PriceLabel({ label, value, isLargeSize }) {
-    return (
-        <Box
-            component={isLargeSize ? 'h4' : 'span'}
-            color='success.light'
-            fontWeight='bold'
-        >
-            {label && <span>{label}</span>}
-
-            R$ { value}
-        </Box >
-    )
+  return (
+    <Box
+      component={isLargeSize ? "h4" : "span"}
+      color="success.light"
+      fontWeight="bold"
+    >
+      {label && <span>{label}</span>}
+      R${value}
+    </Box>
+  );
 }
 
 PriceLabel.propTypes = {
-    label: PropTypes.string,
-    value: PropTypes.number.isRequired,
-    isLargeSize: PropTypes.bool,
+  label: PropTypes.string,
+  value: PropTypes.number.isRequired,
+  isLargeSize: PropTypes.bool,
 };
 
 export default PriceLabel;
